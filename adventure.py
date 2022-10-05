710ca935e115ac72f22d569286f02cc9406eca40
Tyler-I-Kaminer <Tyler.Kaminer1@marist.edu>
2022-10-05 12:31:46 -0400

    Make a new command for revealing location details
    Game now accepts examine command which displays the additional details. Put old code into old_a.py just to have it

--- adventure.py
+++ adventure.py
@@ -1,30 +1,27 @@
 
 counter = 1 
 
-next = "Press ENTER to continue your quest"
 begin = "Press ENTER to begin your quest"
-closer = "Hit enter to take a closer look" 
-
 d_mountain = "You slowly open your eyes and look around, all you see is clouds, there is a path to a cliff to your left, a path leading down straight ahead, and on your right a slope."
-cmountain = "The path on the left leads towards a beach and the one on the right drops down with a pair of skis at the top."
+e_mountain = "The path on the left leads towards a beach and the one on the right drops down with a pair of skis at the top."
 d_beach = "You decide to head down to the left towards the beach that goes in either direction as far as you can see."
-cbeach = "Upon closer inspection you see some chairs and beach umbrellas along the beach, glancing at the ocean you see a giant shadow of a structure looming underwater."
+e_beach = "Upon closer inspection you see some chairs and beach umbrellas along the beach, glancing at the ocean you see a giant shadow of a structure looming underwater."
 d_ocean = "You enter the ocean to swim towards the mysterious underwater building."
-cocean = "Taking a moment to stop and glance around underwater, you see beatiful coral all around almost as if the ocean is glowing. Fish dart in and out of the coral, you notice some sharks in the distance."
+e_ocean = "Taking a moment to stop and glance around underwater, you see beatiful coral all around almost as if the ocean is glowing. Fish dart in and out of the coral, you notice some sharks in the distance."
 d_temple = "Inside the temple it's overgrown with coral, sharks dart in and out of the columns that look like they are about to fall any second."
-ctemple = "Further in, swimming around the decrepit colums, you come upon a dias with a diagram depicitng a pathway to a ship."
+e_temple = "Further in, swimming around the decrepit colums, you come upon a dias with a diagram depicitng a pathway to a ship."
 d_ship = "Following the map, you surface next a half sunken ship, once on board all you see is how time has eaten away at the once beatiful craftsmanship."
-cship = "Looking through the rotten deck of the ship you see the treasure you have been searching for."
+e_ship = "Looking through the rotten deck of the ship you see the treasure you have been searching for."
 d_cliff = "You can see the mast of a ship in the distance and an ocean that stretches as far as the eye can see."
-ccliff = "Around you is rocky terrain dotted with small plants. Behind one of the rocks you see a key glinting in the setting sun."
+e_cliff = "Around you is rocky terrain dotted with small plants. Behind one of the rocks you see a key glinting in the setting sun."
 d_slope = "Going past where you started, taking the path on the right, you hop on the skis and start to descend"
-cslope = "Through the bottom of the clouds you see the snow covered landscape with sun beating down on it and a chairlift in the distance."
+e_slope = "Through the bottom of the clouds you see the snow covered landscape with sun beating down on it and a chairlift in the distance."
 d_lift = "Once at the base of the chairlift, you can see rust covering the chairs."
-clift = "Upon a closer look you see that the door to the control room is open and you spot a keyhole."
+e_lift = "Upon a closer look you see that the door to the control room is open and you spot a keyhole."
 d_lodge ="Stopping right before the parking lot, you enter an old ski lodge."
-clodge = "Once inside you see cans of food and mice scurrying around. Trays and utensils are strewn everywhere, as if people left in a hurry. Grabbing some of the untouched cans of food, you continue on skis down towards the parking lot."
+e_lodge = "Once inside you see cans of food and mice scurrying around. Trays and utensils are strewn everywhere, as if people left in a hurry. Grabbing some of the untouched cans of food, you continue on skis down towards the parking lot."
 d_lot = "You kick off your skis. Running through the parking lot, you glance around seeing how some of the cars are ensnared by vines. One even has a tree going through it. You begin to wonder not where you are but how long it has been."
-clot = "Finally in the second to last row you find a car that stands alone, getting in and flipping down the visor a key falls in your lap. Turning the key in the ignition the car miracously sputters to life."
+e_lot = "Finally in the second to last row you find a car that stands alone, getting in and flipping down the visor a key falls in your lap. Turning the key in the ignition the car miracously sputters to life."
 
 
 
@@ -55,7 +52,8 @@ def game():
     global visited
     initiation()
 #########################################
-    commands = ["north","south","east","west","help","quit"]
+
+    commands = ["north","south","east","west","examine", "help","quit"]
     mountain = "mountain top"
     beach = "beach"
     ocean = "ocean"
@@ -68,6 +66,7 @@ def game():
     lot = "parking lot"
     location = "mountain top"
     visited = [mountain] 
+    
 
 
 
@@ -233,98 +232,35 @@ def game():
                     print("Do you have the treasure you are searching for? If yes type quit as your next direction to break the simulation.")
                 else: 
                     print("You bump into an invisible wall and hear a voice telling you to go in a diffrent direction")
-            elif user_movement == commands[4]: #help
+
+            elif user_movement == commands[4]: #examine
+                if location == mountain:
+                    print(e_mountain)
+                elif location == beach:
+                    print(e_beach)
+                elif location == ocean:
+                    print(e_ocean)
+                elif location == temple:
+                    print(e_temple)
+                elif location == ship:
+                    print(e_ship)
+                elif location == cliff:
+                    print(e_cliff)
+                elif location == slope:
+                    print(e_slope)
+                elif location == lift:
+                    print(e_lift)
+                elif location == lodge:
+                    print(e_lodge)
+                elif location == lot:
+                    print(e_lot)
+                
+            elif user_movement == commands[5]: #help
                 print("Valid commands are north, south, east, west, help, and quit")
-            elif user_movement == commands[5]: #quit
+            elif user_movement == commands[6]: #quit
                 break
         else: 
             print("Please enter a valid command")
             continue
-    ending()
-#########################################
-       # start = input(begin)
-       # counter = counter + 1
-
-       # print(d_mountain) #1
-      #  more = input(closer)
-       # print(cmountain) #1.5
-
-     #   start = input(next)
-     #   counter = counter + 1
-
-     #   print(d_beach) #2
-     #   more = input(closer)
-     #   print(cbeach) #2.5
-
-     #   start = input(next)
-     #   counter = counter + 1
-
-     #   print(d_ocean) #3
-     #   more = input(closer)
-     #   print(cocean) #3.5
-
-
-     #   start = input(next)
-     #   counter = counter + 1
-
-     #  print(d_temple) #4
-     #   more = input(closer)
-     #   print(ctemple) #4.5
-
-
-     #   start = input(next)
-     #   counter = counter + 1
-
-     #   print(d_ship) #5
-     #   more = input(closer)
-     #   print(cship) #5.5
-
-
-     #   start = input(next)
-     #   counter = counter + 1
-
-     #   print(d_cliff) #6
-     #   more = input(closer)
-     #  print(ccliff) #6.5
-
-
-     #  start = input(next)
-     #   counter = counter + 1
-
-     #   print(d_slope) #7
-     #   more = input(closer)
-     #   print(cslope) #7.5
-
-
-     #   start = input(next)
-     #   counter = counter + 1
-
-     #   print(d_lift) #8
-     #   more = input(closer)
-     #   print(clift) #8.5
-
-
-     #   start = input(next)
-     #   counter = counter + 1
-
-     #   print(d_lodge) #9
-     #   more = input(closer)
-     #   print(clodge) #9.5
-
-
-     #   start = input(next)
-     #   counter = counter + 1
-
-     #   print(d_lot) #10
-     #   more = input(closer)
-     #   print(clot) #10.5
-
-
-     #   start = input(next)
-    
-
-
-     #   print("Total number of stops:", counter)
-
-     #   print(credits)
+    ending()     
 game()
---
+++ old_a.py
@@ -0,0 +1,114 @@
+
+counter = 1 
+
+next = "Press ENTER to continue your quest"
+begin = "Press ENTER to begin your quest"
+closer = "Hit enter to take a closer look" 
+
+d_mountain = "You slowly open your eyes and look around, all you see is clouds, there is a path to a cliff to your left, a path leading down straight ahead, and on your right a slope."
+e_mountain = "The path on the left leads towards a beach and the one on the right drops down with a pair of skis at the top."
+d_beach = "You decide to head down to the left towards the beach that goes in either direction as far as you can see."
+e_beach = "Upon closer inspection you see some chairs and beach umbrellas along the beach, glancing at the ocean you see a giant shadow of a structure looming underwater."
+d_ocean = "You enter the ocean to swim towards the mysterious underwater building."
+e_ocean = "Taking a moment to stop and glance around underwater, you see beatiful coral all around almost as if the ocean is glowing. Fish dart in and out of the coral, you notice some sharks in the distance."
+d_temple = "Inside the temple it's overgrown with coral, sharks dart in and out of the columns that look like they are about to fall any second."
+e_temple = "Further in, swimming around the decrepit colums, you come upon a dias with a diagram depicitng a pathway to a ship."
+d_ship = "Following the map, you surface next a half sunken ship, once on board all you see is how time has eaten away at the once beatiful craftsmanship."
+e_ship = "Looking through the rotten deck of the ship you see the treasure you have been searching for."
+d_cliff = "You can see the mast of a ship in the distance and an ocean that stretches as far as the eye can see."
+e_cliff = "Around you is rocky terrain dotted with small plants. Behind one of the rocks you see a key glinting in the setting sun."
+d_slope = "Going past where you started, taking the path on the right, you hop on the skis and start to descend"
+e_slope = "Through the bottom of the clouds you see the snow covered landscape with sun beating down on it and a chairlift in the distance."
+d_lift = "Once at the base of the chairlift, you can see rust covering the chairs."
+e_lift = "Upon a closer look you see that the door to the control room is open and you spot a keyhole."
+d_lodge ="Stopping right before the parking lot, you enter an old ski lodge."
+e_lodge = "Once inside you see cans of food and mice scurrying around. Trays and utensils are strewn everywhere, as if people left in a hurry. Grabbing some of the untouched cans of food, you continue on skis down towards the parking lot."
+d_lot = "You kick off your skis. Running through the parking lot, you glance around seeing how some of the cars are ensnared by vines. One even has a tree going through it. You begin to wonder not where you are but how long it has been."
+e_lot = "Finally in the second to last row you find a car that stands alone, getting in and flipping down the visor a key falls in your lap. Turning the key in the ignition the car miracously sputters to life."
+
+
+# start = input(begin)
+       # counter = counter + 1
+
+       # print(d_mountain) #1
+      #  more = input(closer)
+       # print(cmountain) #1.5
+
+     #   start = input(next)
+     #   counter = counter + 1
+
+     #   print(d_beach) #2
+     #   more = input(closer)
+     #   print(cbeach) #2.5
+
+     #   start = input(next)
+     #   counter = counter + 1
+
+     #   print(d_ocean) #3
+     #   more = input(closer)
+     #   print(cocean) #3.5
+
+
+     #   start = input(next)
+     #   counter = counter + 1
+
+     #  print(d_temple) #4
+     #   more = input(closer)
+     #   print(ctemple) #4.5
+
+
+     #   start = input(next)
+     #   counter = counter + 1
+
+     #   print(d_ship) #5
+     #   more = input(closer)
+     #   print(cship) #5.5
+
+
+     #   start = input(next)
+     #   counter = counter + 1
+
+     #   print(d_cliff) #6
+     #   more = input(closer)
+     #  print(ccliff) #6.5
+
+
+     #  start = input(next)
+     #   counter = counter + 1
+
+     #   print(d_slope) #7
+     #   more = input(closer)
+     #   print(cslope) #7.5
+
+
+     #   start = input(next)
+     #   counter = counter + 1
+
+     #   print(d_lift) #8
+     #   more = input(closer)
+     #   print(clift) #8.5
+
+
+     #   start = input(next)
+     #   counter = counter + 1
+
+     #   print(d_lodge) #9
+     #   more = input(closer)
+     #   print(clodge) #9.5
+
+
+     #   start = input(next)
+     #   counter = counter + 1
+
+     #   print(d_lot) #10
+     #   more = input(closer)
+     #   print(clot) #10.5
+
+
+     #   start = input(next)
+    
+
+
+     #   print("Total number of stops:", counter)
+
+     #   print(credits)
