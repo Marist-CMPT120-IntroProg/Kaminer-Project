d95b220cd08b82d60f54d6bbc958c62610c22450
Tyler-I-Kaminer <Tyler.Kaminer1@marist.edu>
2022-11-22 15:39:25 -0500

    Made talk method in player.py file
    Enabling the player to interact with the npc if there is one there. Made points added if guessed the riddle correctly.

--- player.py
+++ player.py
@@ -39,6 +39,20 @@ class Player:
         else:
             print(new_loc)
             self.current_loc,self.score = self.points(new_loc, self.score)
+
+    def talk(self,score):
+        if self.world.npc[self.world.all_locales.index(self.current_loc)] == None:
+            print("You look around but there is no one in sight")
+        elif self.world.npc[self.world.all_locales.index(self.current_loc)] == 0:
+            riddle_answer = input("What is the answer to Vintons Riddle: ").lower() 
+            if riddle_answer in self.world.answers:
+                self.score += 1000
+                return score
+            else:
+                print("You have guessed wrong, may Davy Jones claim your soul")
+        else:
+            print(self.world.npc[self.world.all_locales.index(self.current_loc)])
+
     
 
 
